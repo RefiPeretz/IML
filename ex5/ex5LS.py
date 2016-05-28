33ba4009d77cc0c8343824db3a0b5031c63684ac
RefiPeretz <refi11991@gmail.com>
2016-05-28 14:05:54 +0300

    final version from lab

--- ex5/ex5LS.py
+++ ex5/ex5LS.py
@@ -1,90 +1,102 @@
 import numpy as np
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
-
-
+#from builtins import range
+from numpy.lib.polynomial import poly1d, polydiv
 from array import *
+import math
 
-
+########## parameters #########
 X = np.load("X_poly.npy")
 Y = np.load("Y_poly.npy")
-Xtrain = X[:99]
-Ytrain = Y[:99]
-Xvalid = X[100:199]
-Yvalid = Y[100:199]
+Xtrain = X[:100]
+Ytrain = Y[:100]
+Xvalid = X[100:200]
+Yvalid = Y[100:200]
 Xtest = X[200:]
 Ytest = Y[200:]
-
-####### training #########
-h_d = [0]
-for i in range(1, 16):
-    h_d.append(np.polyfit(Xtrain,Ytrain,i))
-
-for a in range(0,15):
-    plt.plot(h_d[0])
-    plt.show()
+###############################
 
 def findH(h,x,y):
     list_h_erros = []
     for i in range(0,15):
-        list_h_erros.append(calcError(h[i],x,y,i+1))
+        list_h_erros.append(calcError(h[i],x,y,100))
+    return list_h_erros
 
+    for g in range(0,15):
+        print(list_h_erros[g])
 
-def calcError(h,x,y,i):
-    poly_list = [1]*i
-    error = 0
-    a = np.array(poly_list)
+def calcError(h,x,y,size_list):
     b = np.array(h)
-    for j in range(0,100):
-        1+1
-
-
-
-####### validation ########
-def errorLs(hepo,x,y):
-    LS = 0;
-
-    LS = 0.0025 + 1
-    LS = LS/100
-    return LS
-
-h_loss = []
-for j in range(0, 15):
-    h_loss.append(errorLs(h_d[j],Xtrain,Ytrain))
-
+    error= 0
+    p = poly1d(b)
+    for j in range(0,size_list):
+        error += (math.pow((p(x[j]) - y[j]),2))
+    error= error/size_list
+    return error
 
 ######### training #############
-## todo - choose hepoteza hfinal
-hfinal = 1
-lastError = errorLs(hfinal,Xtest,Ytest)
+h_d = []
+for i in range(1, 16):
+    h_d.append(np.polyfit(Xtrain,Ytrain,i))
+
+###############################
+
+######### validation ##########
 
 
 
 
 
 ############# k - fold cross validation algorithm ###########
-k = 5
-#score = cross_validation(k);
-#Xtrain = X[:99]
-#Ytrain = Y[:99]
-#Xvalid = X[100:199]
-#Yvalid = Y[100:199]
+def kFold(Xpart,Ypart):
+    list_k_x = [Xpart[:40],Xpart[40:80],Xpart[80:120],Xpart[120:160],Xpart[160:200]]
+    list_k_y = [Ypart[:40],Ypart[40:80],Ypart[80:120],Ypart[120:160],Ypart[160:200]]
+    split_1_x = np.append(list_k_x[1],[list_k_x[2],list_k_x[3],list_k_x[4]])
+    split_1_y = np.append(list_k_y[1],[list_k_y[2],list_k_y[3],list_k_y[4]])
+    split_2_x = np.append(list_k_x[0],[list_k_x[2],list_k_x[3],list_k_x[4]])
+    split_2_y = np.append(list_k_y[0],[list_k_y[2],list_k_y[3],list_k_y[4]])
+    split_3_x = np.append(list_k_x[1],[list_k_x[0],list_k_x[3],list_k_x[4]])
+    split_3_y = np.append(list_k_y[1],[list_k_y[0],list_k_y[3],list_k_y[4]])
+    split_4_x = np.append(list_k_x[1],[list_k_x[2],list_k_x[0],list_k_x[4]])
+    split_4_y = np.append(list_k_y[1],[list_k_y[2],list_k_y[0],list_k_y[4]])
+    split_5_x = np.append(list_k_x[1],[list_k_x[2],list_k_x[3],list_k_x[0]])
+    split_5_y = np.append(list_k_y[1],[list_k_y[2],list_k_y[3],list_k_y[0]])
 
+    split_i_list_x = [split_1_x,split_2_x,split_3_x,split_4_x,split_5_x]
+    split_i_list_y = [split_1_y,split_2_y,split_3_y,split_4_y,split_5_y]
+    h_all_error_train = []
+    h_all_error_validation = []
+    for i in range(1,16):
+        error_h_train = 0
+        error_h_validation = 0
+        for j in range(0,5):
+            h = np.polyfit(split_i_list_x[j],split_i_list_y[j],i)
+            error_h_train += calcError(h,Xpart,Ypart,200)
+            error_h_validation += calcError(h,list_k_x[j],list_k_y[j],40)
+        erro_h_train = error_h_train/5
+        error_h_validation = error_h_validation/5
+        print(erro_h_train,error_h_validation)
+        h_all_error_train.append(erro_h_train)
+        h_all_error_validation.append(error_h_validation)
 
+    plt.xlabel('blaa')
+    plt.ylabel("blaaaaa")
+    plt.title("Least Squares algorithm")
+    plt.plot(range(1,16),h_all_error_train,'r', label='training')
+    plt.plot(range(1,16),h_all_error_validation,'g', label="validation")
+    plt.show()
 
-####### random ##########
-# np.random.uniform(0,1,1000)
-# np.random.normal(mu,sigma,1000)
+############# ploting ####################
+list_erros_train = findH(h_d,Xtrain,Ytrain)
+list_erros_valid = findH(h_d,Xvalid,Yvalid)
+list_erros_test = findH(h_d,Xtest,Ytest)
+plt.xlabel('digree')
+plt.ylabel("error")
+plt.title("Least Squares algorithm")
+plt.plot(range(1,16),list_erros_train,'r', label='training')
+plt.plot(range(1,16),list_erros_valid,'g', label="validation")
+plt.plot(range(1,16),list_erros_test,'b',label="testing")
+plt.show()
 
-####### operations #######
-# np.linalg.pinv(A)
-# np.column_stack((u,v))
-# np.concatenate((a,b))
-
-
-######## ploting ##########
-#plt.plot(X)
-#plt.scatter(X,Y)
-# plt.show()
-#print(Xtrain)
-#print(Ytrain)
+kFold(X[:200],Y[:200])
